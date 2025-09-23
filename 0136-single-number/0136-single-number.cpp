72a453b1274c598704cb8c873563e6d4ededf65f
shubham1716 <shubhamk7640@gmail.com>
2025-09-23 11:23:14 -0400

    Time: 12 ms (13.14%), Space: 23.8 MB (12.09%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,11 +1,43 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int n=nums.size();
-        int xo=0;
-        for(int i=0;i<n;i++){
-         xo ^= nums[i];
+        // int n=nums.size();
+        // int xo=0;
+        // for(int i=0;i<n;i++){
+        //  xo ^= nums[i];
+        // }
+        // return xo;
+        // sort(nums.begin(),nums.end());
+        // int i=0;
+        // int n=nums.size();
+        // int ans=-1;;
+        // while(i<n){
+        //     if(i+1<n && nums[i]==nums[i+1]){
+        //     i = i+2;}
+            
+        //     else{
+        //         ans=nums[i];
+        //         break;
+
+        //     }
+
+        // }
+        // return ans;
+        unordered_map<int,int>freqmap;
+        for(int i=0;i<nums.size();i++){
+            int num =nums[i];
+            freqmap[num]=freqmap[num]+1;
+        } 
+        // int ans;
+        // unordered_map<int,int>::iterator it;
+        for(auto it:freqmap){
+        int key=it.first;
+        int value=it.second;
+        if(value==1){
+      return key;
         }
-        return xo;
-    }
+        }
+        return -1;
+    }  
+
 };
