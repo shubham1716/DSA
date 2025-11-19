8a96e67eb4f26050a556110a2c058ff958c980cd
shubham1716 <shubhamk7640@gmail.com>
2025-11-19 09:43:32 -0500

    Time: 0 ms (100%), Space: 8.6 MB (14.77%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        
+
+        long low = 0, high = x, ans = 0;
+
+        while (low <= high) {
+            long mid = low + (high - low) / 2;
+
+            if (mid * mid <= x) {
+                ans = mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return (int) ans;
+    }
+
+
+    
+};
